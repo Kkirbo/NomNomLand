79a55741040701c10c6f30608acd8a997e8d154c
weerotmd <weerotmd@gmail.com>
2026-02-17 23:57:35 +0100

    stylised footer and linked contact information

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,6 @@
+/**
+ * Carousel
+ */
 const carousel = document.querySelector('.carousel');
 const cards = [...carousel.children];
 const firstValidCard = 1;
@@ -29,6 +32,28 @@ carousel.addEventListener('scroll', () => {
 window.addEventListener('load', () => {
   cards[firstValidCard].scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
 });
+
+/**
+ * Searchbar
+ */
+const searchbar = document.querySelector('.searchbar');
+const searchinput = searchbar.querySelector('input');
+searchinput.placeholder = "";
+const placeholderText = "Search for your favorite meal...";
+function animatePlaceholder() {
+  if (searchinput.placeholder.length == placeholderText.length) {
+   return;
+  } else searchinput.placeholder += placeholderText[searchinput.placeholder.length];
+  window.setTimeout(animatePlaceholder, Math.random() * (250 - 50) + 50);
+};
+const searchBarEntersView = new IntersectionObserver(() => {
+  searchinput.placeholder = "";
+  animatePlaceholder();
+}, 
+{
+  threshold: 1
+});
+searchBarEntersView.observe(searchbar);
 
 
 /**
