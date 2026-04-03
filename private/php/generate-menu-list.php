48a2c281e92d83b03cc79776f920778e3fe1f981
cyjulien <julien.renard@etu.cyu.fr>
2026-04-03 11:21:11 +0200

    Add files via upload

--- private/php/generate-menu-list.php
+++ private/php/generate-menu-list.php
@@ -1,20 +1,23 @@
 <?php
-$menus = json_decode(file_get_contents('../../private/data/menus.json'), true);
+function generateMenuList($count=0)
+{
+  $menus = json_decode(file_get_contents('../../private/data/menus.json'), true);
 
-$menus = array_slice($menus, 0, 3);
+  if ($count>0) $menus = array_slice($menus, 0, $count);
 
-foreach ($menus as $menu) {
-    echo '<article class="menu modernNeonBoxGlass">';
-    echo '<a href="menu.php#' . htmlspecialchars($menu['id']) . '" class="imageLink">';
-    echo '<img draggable="false" src="' . htmlspecialchars($menu['image']) . '" alt="' . htmlspecialchars($menu['title']) . ' Image">';
-    echo '</a>';
-    echo '<h3>' . htmlspecialchars($menu['title']) . '</h3>';
-    echo '<ul>';
-    foreach ($menu['description'] as $desc) {
-        echo '<li>' . htmlspecialchars($desc) . '</li>';
-    }
-    echo '</ul>';
-    echo '<a href="menu.php#' . htmlspecialchars($menu['id']) . '"><button>' . htmlspecialchars($menu['price']) . '€</button></a>';
-    echo '</article>';
+  foreach ($menus as $menu) {
+      echo '<article class="menu modernNeonBoxGlass">';
+      echo '<a href="menu.php#' . htmlspecialchars($menu['id']) . '" class="imageLink">';
+      echo '<img draggable="false" src="' . htmlspecialchars($menu['image']) . '" alt="' . htmlspecialchars($menu['title']) . ' Image">';
+      echo '</a>';
+      echo '<h3>' . htmlspecialchars($menu['title']) . '</h3>';
+      echo '<ul>';
+      foreach ($menu['description'] as $desc) {
+          echo '<li>' . htmlspecialchars($desc) . '</li>';
+      }
+      echo '</ul>';
+      echo '<a href="menu.php#' . htmlspecialchars($menu['id']) . '"><button>' . htmlspecialchars($menu['price']) . '€</button></a>';
+      echo '</article>';
+  }
 }
 ?>
