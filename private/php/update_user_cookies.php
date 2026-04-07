db46133c806bcbe4a813f5d15976db47f5cf2f9d
cyjulien <julien.renard@etu.cyu.fr>
2026-04-07 16:14:05 +0200

    Add files via upload

--- private/php/update_user_cookies.php
+++ private/php/update_user_cookies.php
@@ -1,38 +1,58 @@
 <?php
 
+require '../../private/php/session.php';
+require_login();
+$loggedInUser = get_user_by_session();
 
 require_once "./data_loader.php";
 
-$users = getUsers();
+$path = __DIR__ . "/../data/users.json";
 
-$userId = $_POST['userId'] ?? null;
-$essentialCookies = $_POST['essentialCookies'] ?? null;
+if (file_exists($path)) {
+    $content = file_get_contents($path);
+    $data = json_decode($content, true);
+
+    if (!isset($data['users']) || !is_array($data['users'])) {
+        $data['users'] = [];
+    }
+} else {
+    $data = ["users" => []];
+}
+
 $essentialCookies = true;
-$analyticsCookies = $_POST['analyticsCookies'] ?? null;
-$functionalCookies = $_POST['functionalCookies'] ?? null;
+$analyticsCookies = isset($_POST['analyticsCookies']);
+$functionalCookies = isset($_POST['functionalCookies']);
 
-
-foreach($users as &$user){
-
-    if ($user["id"] === $userId) {
-        
-        $user["cookies"] = array(
-            "essentialCookies" => $essentialCookies == 'on',
-            "analyticsCookies" => $analyticsCookies == 'on',
-            "functionalCookies" => $functionalCookies == 'on'
-        );
-
+$found = false;
+foreach ($data['users'] as &$user) {
+    if ($user['id'] === $loggedInUser['id']) {
+        $user['cookies'] = [
+            "essentialCookies" => $essentialCookies,
+            "analyticsCookies" => $analyticsCookies,
+            "functionalCookies" => $functionalCookies
+        ];
+        $found = true;
         break;
     }
 }
 
-unset($user);
+if (!$found) {
+    $loggedInUser['cookies'] = [
+        "essentialCookies" => $essentialCookies,
+        "analyticsCookies" => $analyticsCookies,
+        "functionalCookies" => $functionalCookies
+    ];
+    $data['users'][] = $loggedInUser;
+}
 
-$users = array(
-    "users" => $users
-);
+$json = json_encode($data, JSON_PRETTY_PRINT);
+if ($json === false) {
+    die("JSON Error: " . json_last_error_msg());
+}
 
-file_put_contents("../../private/data/users.json", json_encode($users, JSON_PRETTY_PRINT));
+if (file_put_contents($path, $json) === false) {
+    die("Internal server error: could not save cookies.");
+}
 
 header("Location: ../../public/views/profile.php");
 exit();
