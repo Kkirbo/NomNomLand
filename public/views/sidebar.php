f35db84074c83fb618183bac139d177b71607559
cyjulien <julien.renard@etu.cyu.fr>
2026-04-14 16:52:11 +0200

    Add files via upload

--- public/views/sidebar.php
+++ public/views/sidebar.php
@@ -76,6 +76,15 @@
           </li>
         ';
         $user = get_user_by_session();
+        if ($user && ($user['role'] === 'admin' || $user['role'] === 'cook' || $user['role'] === 'delivery'))
+        echo '
+          <li>
+            <a href="orders.php">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M160-160v-516L82-846l72-34 94 202h464l94-202 72 34-78 170v516H160Zm240-280h160q17 0 28.5-11.5T600-480q0-17-11.5-28.5T560-520H400q-17 0-28.5 11.5T360-480q0 17 11.5 28.5T400-440ZM240-240h480v-358H240v358Zm0 0v-358 358Z"/></svg>
+              Orders
+            </a>
+          </li>
+        ';
         if ($user && $user['role'] === 'admin')
         echo '
           <li>
