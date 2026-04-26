718d4a7a71536ddafe103b7b1cc8b6ca2eeedd89
ccy946 <clement.schneider@etu.u-cergy.fr>
2026-04-26 16:11:59 +0200

    Add files via upload

---
+++ private/php/update_profile.php
@@ -0,0 +1,64 @@
+<?php
+require '../../private/php/session.php';
+require '../../private/php/data_loader.php';
+
+header('Content-Type: application/json');
+require_login();
+
+$loggedInUser = get_user_by_session();
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$field = $data['field'] ?? null;
+$value = $data['value'] ?? null;
+
+if (!$field || !$value) {
+    echo json_encode(["success" => false, "error" => "Missing data"]);
+    exit;
+}
+
+// whitelist des champs modifiables
+$profileKeys = ["firstName", "lastName", "username", "address"];
+$nonProfileKeys = ["phone", "email"];
+
+$allowedFields = array_merge($profileKeys, $nonProfileKeys);
+
+if (!in_array($field, $allowedFields)) {
+    echo json_encode(["success" => false, "error" => "Invalid field"]);
+    exit;
+}
+
+
+$path = __DIR__ . "/../data/users.json";
+$content = file_get_contents($path);
+$users_data = json_decode($content, true);
+
+if ($field == "email") {
+    $_SESSION["user_email"] = $value;
+}
+
+if (in_array($field, $profileKeys)) {
+
+    foreach($users_data["users"] as &$user) {
+        if ($user["email"] == $loggedInUser["email"]) {
+            $user["profile"][$field] = $value;
+        }
+    }
+
+} else {
+
+    foreach($users_data["users"] as &$user) {
+        if ($user["email"] == $loggedInUser["email"]) {
+            $user[$field] = $value;
+        }
+    }
+
+}
+
+
+$json = json_encode($users_data, JSON_PRETTY_PRINT);
+file_put_contents($path, $json);
+
+
+echo json_encode(["success" => true]);
+?>
