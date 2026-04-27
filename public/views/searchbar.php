2791b4ba39932d3441d281f11c5240edfc12d7dd
weerotmd <weerotmd@gmail.com>
2026-04-27 05:32:30 +0200

    CSS fix for searchbar

--- public/views/searchbar.php
+++ public/views/searchbar.php
@@ -9,18 +9,26 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z"/></svg>
     </button>
     <section class="dropdown">
-        <button type="button">Price ↓</button>
-        <button type="button">Most bought ↓</button>
+        <article>
+            <button type="button">Price ↓</button>
+            <button type="button">Most bought ↓</button>
+        </article>
 
-        <label><input type="checkbox" name="starters">Starters</label>
-        <label><input type="checkbox" name="maincourses">Main courses</label>
-        <label><input type="checkbox" name="drinks">Drinks</label>
-        <label><input type="checkbox" name="desserts">Desserts</label>
+        <h3>Filters</h3>
+        <article>
+            <label><input type="checkbox" name="starters">Starters</label>
+            <label><input type="checkbox" name="maincourses">Main courses</label>
+            <label><input type="checkbox" name="drinks">Drinks</label>
+            <label><input type="checkbox" name="desserts">Desserts</label>
+        </article>
+
+        <article>
+            <label><input type="checkbox" name="vegan">Vegan</label>
+            <label><input type="checkbox" name="spice">Spice</label>
+        </article>
         
-        <label><input type="checkbox" name="vegan">Vegan</label>
-        <label><input type="checkbox" name="spice">Spice</label>
-        <details>
-            <summary>Allergens</summary>
+        <h3>Allergens</h3>
+        <article>
             <label><input type="checkbox" name="allergens[]" value="gluten"> Gluten</label>
             <label><input type="checkbox" name="allergens[]" value="milk"> Milk</label>
             <label><input type="checkbox" name="allergens[]" value="eggs"> Eggs</label>
@@ -32,6 +40,6 @@
             <label><input type="checkbox" name="allergens[]" value="mustard"> Mustard</label>
             <label><input type="checkbox" name="allergens[]" value="sesame"> Sesame</label>
             <label><input type="checkbox" name="allergens[]" value="sulphites"> Sulphites</label>
-        </details>
+        </article>
     </section>
 </form>
