3b08272c1549169ca67edf04230c786e006011dd
Kkirbo <aymaneamara935@gmail.com>
2026-04-28 03:03:16 +0200

    Implement user last login tracking: update lastLogin timestamp in users.json upon successful login

--- private/php/login.php
+++ private/php/login.php
@@ -1,4 +1,6 @@
 <?php
+$path ="../data/users.json";
+$data = json_decode(file_get_contents($path), true);
 if (isset($_SESSION["user_email"])) header("Location: index.php");
 $error = '';
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') return;
@@ -11,4 +13,11 @@ $login = login($_POST['email'], $_POST['password']);
 
 if ($login == 1) $error = "Please fill in all fields.";
 else if ($login == 2) $error = "Invalid credentials, please try again.";
+ foreach ($data['users'] as &$user) {
+            if ($user['email'] === $email) {
+                $user['lastLogin'] = date("Y-m-d H:i:s");
+                break;
+            }
+    file_put_contents($path, json_encode($data, JSON_PRETTY_PRINT));
+ }
 ?>
