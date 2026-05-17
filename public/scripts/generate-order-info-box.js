32bf69193c3485c1d3e06d837260b02106af3258
weerotmd <weerotmd@gmail.com>
2026-05-17 23:57:19 +0200

    [Phase#3] FIN

--- public/scripts/generate-order-info-box.js
+++ public/scripts/generate-order-info-box.js
@@ -58,28 +58,3 @@ export async function generateOrderInfoBox(orderInfo, showRatingLink=false) {
     box.innerHTML = newHTML;
     return box;
 }
-
-/*
-async function refreshOrderPreview() {
-const res = await fetch("../api/get-latest-command.php");
-    const data = await res.json();
-    if (!data.success) return;
-    const container = document.getElementById("order-container");
-    if (!container) return;
-    let html = "";
-    for (const dish of data.dishes) {
-    html += `
-        <div class="order-preview">
-            <img src="${dish.image}" alt="${dish.title}">
-            <div class="infos">
-                <span class="title">${dish.title}</span>
-                <span class="meta">${dish.version}</span>
-                <span class="price">x${dish.quantity} • ${dish.price}€</span>
-            </div>
-        </div>
-    `;
-    }
-    container.innerHTML = html;
-}
-setInterval(refreshOrderPreview, 3000);
-refreshOrderPreview();*/
